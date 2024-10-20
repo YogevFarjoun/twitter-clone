925bcba261506e0e40379e7df070717d263e4373
YogevFarjoun <yogevfarjoun@gmail.com>
2024-10-20 11:15:36 +0300

    1 - notification routes and controllers added

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,32 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: "from",
+      select: "username profileImg",
+    });
+
+    await Notification.updateMany({ to: userId }, { read: true });
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.error("Error in getNotifications: ", error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: "Notifications deleted" });
+  } catch (error) {
+    console.error("Error in deleteNotifications: ", error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
